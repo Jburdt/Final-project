e06acf6ceddcad7fde8e010f259bb5e2e3832059
Josh Burd <Jburdt@gmail.com>
2023-04-12 15:04:12 -0400

    added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :comment_reviews, through: comments, source: :review 
+
+  has_secure_password
 end
--- db/migrate/20230406204541_create_reviews.rb
+++ db/migrate/20230406204541_create_reviews.rb
@@ -3,7 +3,7 @@ class CreateReviews < ActiveRecord::Migration[6.1]
     create_table :reviews do |t|
       t.string :title
       t.text :content
-
+      t.integer :user_id
       t.timestamps
     end
   end
