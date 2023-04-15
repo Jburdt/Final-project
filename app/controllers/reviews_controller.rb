40921fcf32dd87521e4d50b033c1ad9d7467c4d2
Josh Burd <Jburdt@gmail.com>
2023-04-15 16:21:40 -0400

    FULL CRUD FOR REVIEWS

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,49 @@
 class ReviewsController < ApplicationController
+
+  # SHOWS ALL REVIEWS
+  def index
+    reviews = Review.all
+    render json: reviews, status: :ok
+  end
+
+  # SHOWS ONE REVIEW BY ID
+  def show
+    review = find_review
+    render json: review
+  end
+
+  # CREATE METHOD IS BROKEN 
+  def create
+    # byebug
+    new_review = Review.create!(review_params)
+    render json: new_review, status: :created
+  end
+
+  # DELETES REVIEW
+  def destroy
+    byebug
+    deleted_review = find_review
+    deleted_review.destroy
+    head :no_content
+  end
+
+  # UPDATES REVIEW
+  def update
+    updated_review = find_review
+    updated_review.update(review_params)
+    render json: updated_review, status: :ok
+  end
+
+  private
+
+  # PARAMS FOR REVIEW
+  def review_params
+    params.permit(:title, :content, :user, :category, :image)
+  end
+
+  # FINDS ONE REVIEW
+  def find_review
+    one_review = Review.all.find(params[:id])
+  end
+
 end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,3 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :user_id
+  attributes :id, :title, :content, :user_id, :image, :category
 end
