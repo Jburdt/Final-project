01b85970ae037fbc232d6b9c3b4ca82dc26c46bc
Josh Burd <Jburdt@gmail.com>
2023-04-20 10:24:31 -0400

    added login link/page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { useSelector  } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Error from "./components/Error";
 import Home from "./components/Home";
+import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import NewReviewForm from "./components/NewReviewForm";
 import Reviews from "./components/Reviews";
@@ -20,7 +21,7 @@ const App = () => {
       <Route path="/reviews" element={ <Reviews /> } />
       <Route path="/reviews/new" element={ <NewReviewForm /> } />
       <Route path="/signup" element={ <Signup /> } />
-      <Route path="/login" element={null } />
+      <Route path="/login" element={ <Login /> } />
       <Route path="*" element={ <Error /> } />
     </Routes>
    </Router>
---
+++ client/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Loging</h1>
+    </div>
+  )
+}
+
+export default Login;
